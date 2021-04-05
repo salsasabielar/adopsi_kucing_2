9b6af440ff1638eccd473bce68687695281ad313
= <=>
2021-04-05 21:50:15 +0700

    mengedit main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,41 @@
 //kode utama Aplikasi tampilan awal
 import 'package:flutter/material.dart';
-import 'home.dart'; //package letak folder Anda
+import 'home.dart';
+import 'homeCustomer.dart'; //package letak folder Anda
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Tambahkan Item',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: Home(),
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Adopsi Kucing"),
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  text: "Kucing",
+                ),
+                Tab(text: "Adopter"),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Home(),
+              HomeCustomer(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
